13b13dbf9c3f09e6cf146ec78f5076322e70039e
John Doe <dl@progears.ru>
2018-06-25 15:46:11 +0300

    fxs

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ module.exports = {
     database: 'mongodb://verslauser:pwd222@46.161.39.213/versla',
     port: 3030,
     secret: "versla27052017"
-}
+};
--- utils/db/db.comment.js
+++ utils/db/db.comment.js
@@ -1,5 +1,5 @@
 const Comment = require('../../models/comment');
-
+const mongoose = require('mongoose');
 module.exports = {
     /**
      * Add comment
@@ -10,18 +10,19 @@ module.exports = {
     addComment: async (commentData, userId) => {
         let comment = new Comment();
 
-        comment.creator_id = userId;
+        comment.creator_id = mongoose.Types.ObjectId(userId);
         if (commentData.text) comment.text = commentData.text;
         if (commentData.title) comment.title = commentData.title;
         if (commentData.type) comment.type = commentData.type;
 
         if (commentData.type === 1) {
-            if (commentData.good_id) comment.good_id = commentData.good_id;
+            if (commentData.good_id) comment.good_id = mongoose.Types.ObjectId(commentData.good_id);
         } else if (commentData.type === 2) {
-            if (commentData.comment_id) comment.comment_id = commentData.comment_id;
+            if (commentData.comment_id) comment.comment_id = mongoose.Types.ObjectId(commentData.comment_id);
         }
 
         comment.save();
+
         return {
             meta: {
                 code: 200,
