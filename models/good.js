2c70210e14792a7471f699d1ceccf7df4ed0b92f
Denis <denis@localhost.localdomain>
2018-06-28 14:40:28 +0300

    chngs

--- models/good.js
+++ models/good.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const autoIncrement = require('mongoose-plugin-autoinc').autoIncrement;
 const mongoolia = require('mongoolia').default;
-const mongooseAlgolia = require('mongoose-algolia');
 
 const GoodSchema = new Schema({
     good_id: {
@@ -48,29 +47,9 @@ GoodSchema.plugin(mongoolia, {
     appId: '1BL49RG52N',
     apiKey: 'fd368d8e55e2feea7d8e447cb2653ed9',
     indexName: 'dev_versla'
-}).plugin(mongooseAlgolia,{
-    appId: '1BL49RG52N',
-    apiKey: 'fd368d8e55e2feea7d8e447cb2653ed9',
-    indexName: 'dev_versla',
-    selector: '-author',
-    defaults: {
-        author: 'unknown'
-    },
-    mappings: {
-        title: function(value) {
-            return `:${value}`
-        }
-    },
-    filter: function(doc) {
-        return !doc.softdelete
-    },
-    debug: true
 });
 
 let Model = mongoose.model('Good', GoodSchema);
-Model.SyncToAlgolia();
-Model.SetAlgoliaSettings({
-    searchableAttributes: ['name','tags','short_description'] //Sets the settings for this schema, see [Algolia's Index settings parameters](https://www.algolia.com/doc/api-client/javascript/settings#set-settings) for more info.
-});
+
 module.exports = Model;
 
