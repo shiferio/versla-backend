86535faa694db21eacda3a8c0a5cc85ee1b989cb
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-01 17:51:16 +0300

    Implement search API that finds in any indexed fields

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const Good = require('../models/good');
 
 
-router.get('/', (req, res) => {
+router.get('/all/:pageNumber/:pageSize', (req, res) => {
     const filter = {};
     if (req.query.name) {
         filter['name'] = { '$regex': new RegExp(req.query.name, 'i') };
@@ -22,8 +22,50 @@ router.get('/', (req, res) => {
         };
     }
 
-    const pageNumber = req.query.pageNumber || 0;
-    const pageSize = req.query.pageSize || 6;
+    const pageNumber = req.params.pageNumber;
+    const pageSize = req.params.pageSize;
+
+    const exclude = {
+        params: 0,
+        creator_id: 0,
+        __v: 0
+    };
+
+    Good
+        .find(filter, exclude)
+        .hint('goods_search')
+        .skip(pageNumber > 0 ? ((pageNumber - 1) * pageSize) : 0)
+        .limit(pageSize)
+        .exec((err, goods) => {
+            res.json({
+                meta: {
+                    code: 200,
+                    success: true,
+                    message: "Successfully get goods"
+                },
+                data: {
+                    goods: goods
+                }
+            })
+        })
+});
+
+router.get('/any/:pageNumber/:pageSize', (req, res) => {
+    const query = (req.query.query || '')
+        .split(new RegExp('\\s'))
+        .map(tag => new RegExp(tag, 'i'));
+
+    const filter = {
+        '$or': [
+            { name: { '$in': query } },
+            { description: { '$in': query } },
+            { short_description: { '$in': query } },
+            { tags: { '$in': query } }
+        ]
+    };
+
+    const pageNumber = Number.parseInt(req.params.pageNumber);
+    const pageSize = Number.parseInt(req.params.pageSize);
 
     const exclude = {
         params: 0,
