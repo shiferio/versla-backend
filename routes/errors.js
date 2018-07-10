fbd90281218f4ba6b39cb9a50f760c92fbebe7de
Denis <dl@progears.ru>
2018-07-10 19:09:27 +0300

    update for post

--- routes/errors.js
+++ routes/errors.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const dbRequests = require('../utils/db/db.requests');
 
-router.route('/add', async (req, res) => {
+router.post('/add', async (req, res) => {
     let data = await dbRequests.addError(req.body);
     return res.status(data['meta'].code).send(data);
 });
