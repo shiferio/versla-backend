bd4ccb23aa2d3dd559781141137ffa7f78f8a49f
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-24 11:02:23 +0300

    Update MongoDB URI for default database (dev API specific)

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const env = require('node-env-file');
 const NODE_ENV = process.env.NODE_ENV || 'production';
 
 if (NODE_ENV === 'production') {
-    module.exports.database = process.env.MONGODB_URI || 'mongodb://versler:versla88@ds235251.mlab.com:35251/versla';
+    module.exports.database = process.env.MONGODB_URI || 'mongodb://owner:pwd222@ds159110.mlab.com:59110/versla_db';
     module.exports.port = process.env.PORT || 3030;
 } else {
     env('.env');
