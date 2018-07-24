2c814573bfce0d9db62398a347e7465f37703f7f
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-24 10:41:48 +0300

    Employ 'production' environment by default

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const env = require('node-env-file');
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || 'production';
 
 if (NODE_ENV === 'production') {
     module.exports.database = process.env.MONGODB_URI || 'mongodb://versler:versla88@ds235251.mlab.com:35251/versla';
